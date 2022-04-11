66b37cc381d1bf262273723d038e8c56588ae166
spoose <spooseageese@gmail.com>
2022-04-10 18:16:52 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5001;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+const path = require('path');
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+// app.get('/', (req,res) => {
+//     console.log("/////")
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.listen(port, () => {
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log(`Server is running on port: ${port}`);
+});
